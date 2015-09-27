6e199bee95d5cc89cb082ea5147d588cd5835e9f
gVien <wavetech86@gmail.com>
2015-09-26 20:25:33 -0700

    update signup and routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var signup_shelter = require("./routes/users")
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use("/signup_shelter", signup_shelter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,7 +58,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
- console.log("WERWRWR");
 
 module.exports = app;
