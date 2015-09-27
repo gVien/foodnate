97c37b9569f1c9fe592c4bfb2ebbdc28ea749164
gVien <wavetech86@gmail.com>
2015-09-26 19:26:15 -0700

    add front end sign un & index page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -12,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -56,5 +57,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
+ console.log("WERWRWR");
 
 module.exports = app;
