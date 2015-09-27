b9abc55e0d6d5999af60f7ad5054e66bb9a2a5cd
Amir Fayek <afayek88@gmail.com>
2015-09-27 11:19:51 -0700

    add user files and page

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var User = require('../models/users');
 
 /* GET users listing. */
+/* GET Userlist page. */
+router.get('/', function(req, res) {
+  res.render('user')
+});
+
+router.get('/donate', function(req, res) {
+  res.render('donate')
+});
+
 router.get('/signup_shelter', function(req, res, next) {
   res.render('signup_shelter', {
     title: "TITLE!!"
